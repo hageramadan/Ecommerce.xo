105427952820651f169626d8368a4baee272cc63
Hager Ramadan <hagerramadan440@gmail.com>
2025-11-02 02:44:47 +0200

    k

--- app/myAccount/page.tsx
+++ app/myAccount/page.tsx
@@ -11,7 +11,8 @@ export default function Page() {
         return (
           <>
             <h2 className="text-2xl font-semibold text-pro mb-6">حسابي</h2>
-            <form className="grid md:grid-cols-2 gap-6">
+            <hr className="bg-gray-50 text-gray-300 mb-5" />
+            <form className="grid md:grid-cols-2 gap-6 my-5">
               <div className="flex flex-col">
                 <label className="mb-1 font-medium">الإسم الأول</label>
                 <input
@@ -29,6 +30,44 @@ export default function Page() {
                 />
               </div>
             </form>
+            <h2 className="text-2xl font-semibold text-pro mb-6">الحماية</h2>
+            <hr className="bg-gray-50 text-gray-300 mb-5" />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                 <div className="flex items-center justify-between py-4">
+              <div>
+                <h3 className="text-lg font-medium text-gray-800">
+                  كلمة المرور
+                </h3>
+                <p className="text-gray-500 text-lg p-0.5 rounded bg-gray-200 my-1">********</p>
+              </div>
+              <button className="text-pro font-medium hover:underline">
+                تغيير
+              </button>
+            </div>
+
+           
+            <div className="flex items-center justify-between py-4">
+              <div>
+                <h3 className="text-lg font-medium text-gray-800">
+                  رقم الهاتف
+                </h3>
+                <p className="text-gray-500 text-lg p-0.5 rounded bg-gray-200 my-1">+9086868</p>
+              </div>
+              <button className="text-pro font-medium hover:underline">
+                تعديل
+              </button>
+            </div>
+            </div>
+            <h2 className="text-2xl font-semibold text-pro mb-6">الاخبار</h2>
+            <hr className="bg-gray-50 text-gray-300 mb-5" />
+
+            <p className="pb-9">انت لست مشترك في النشرات الاخباريه بعد</p>
+            
+          <h2 className="text-2xl font-semibold text-pro mb-6">حذف الحساب</h2>
+            <hr className="bg-gray-50 text-gray-300 mb-5" />
+          <p>نأسف لمغادرتك, انت دائما مرحب بعودتك!</p>
+          <button className="text-red-500 font-medium underline">حذف الحساب</button>
+           
           </>
         );
       case "orders":
@@ -47,7 +86,7 @@ export default function Page() {
       <div className=" col-span-12 md:col-span-2 ">
         <SideBar onSelect={setSection} active={section} />
       </div>
-      <div  className="col-span-12 md:col-span-10 bg-white rounded-2xl shadow p-8 md:p-12 mb-16 w-full">
+      <div className="col-span-12 md:col-span-10 bg-white rounded-2xl shadow p-8 md:p-12 mb-16 w-full">
         {renderContent()}
       </div>
     </div>
